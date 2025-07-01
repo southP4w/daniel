9b3714d0aff76a004d3afee8d64b8b7de1577d0e
southPaw <dangargiullo@gmail.com>
2025-06-30 20:07:50 -0400

    Add `dist` files.

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import './styles.css'
 
 createRoot(document.getElementById('root')).render(
     <StrictMode>
-        <BrowserRouter basename={"/daniel"}>
+        <BrowserRouter basename={"/daniel/"}>
             <App/>
         </BrowserRouter>
     </StrictMode>,
